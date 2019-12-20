affb0e611278436ce922db024b240ff7e0d06d65
denverwilliams <denver@debian.nz>
2019-12-19 19:20:33 -0500

    #crosscloudci/crosscloudci#194 #vulk/cncf_ci/issues/322 test project now uses circle ci

--- test/lib/yml_reader.gitlab_ci_test.exs
+++ test/lib/yml_reader.gitlab_ci_test.exs
@@ -81,7 +81,8 @@ defmodule ExCiProxy.YmlReader.GitlabCiTest do
     end)
 
     assert Enum.find_value(project_list, fn(x) -> x["yml_name"] == "testproj" end) 
-    assert Enum.find_value(project_list, fn(x) -> Enum.at(x["ci_system"], 0)["ci_system_type"] == "github-actions" end) 
+    # assert Enum.find_value(project_list, fn(x) -> Enum.at(x["ci_system"], 0)["ci_system_type"] == "github-actions" end) 
+    assert Enum.find_value(project_list, fn(x) -> Enum.at(x["ci_system"], 0)["ci_system_type"] == "circle-ci" end) 
   end
 
   test "project_list" do 
